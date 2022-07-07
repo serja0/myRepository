49c17da94a1750c1636bbdcd4ad80562d5953246
Serjao <rakih14@gmail.com>
2022-07-07 17:53:41 +0300

    create tabBarViewController

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -10,9 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let tbVC = UITabBarController()
+        tbVC.view.backgroundColor = .gray
+        let frame = UIScreen.main.bounds
+        self.window = UIWindow(frame: frame)
+        self.window?.rootViewController = tbVC
+        window?.makeKeyAndVisible()
+        
         return true
     }
 

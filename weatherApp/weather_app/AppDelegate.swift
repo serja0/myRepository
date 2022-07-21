d4c2cfe73c09d1fbd7ff43465b5ca2cd810b1ae9
Serjao <rakih14@gmail.com>
2022-07-21 13:41:31 +0300

    Prepare project

--- weatherApp/weather_app/AppDelegate.swift
+++ weatherApp/weather_app/AppDelegate.swift
@@ -10,10 +10,18 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let frame = UIScreen.main.bounds
+        let vc = ViewController()
+        let navVC = UINavigationController(rootViewController: vc)
+        self.window = UIWindow(frame: frame)
+        window?.rootViewController = navVC
+        window?.makeKeyAndVisible()
         return true
+        
     }
 
     
